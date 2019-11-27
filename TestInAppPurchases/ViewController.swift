faca9104c0c5df400c6eaf85082091f05453a88d
Jordy van Kuijk <jordy.vankuijk@deptagency.com>
2019-11-27 11:52:58 +0100

    Added demo functionality

--- TestInAppPurchases/ViewController.swift
+++ TestInAppPurchases/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import StoreKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SKProductsRequestDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loadProducts(sender: UIButton) {
+        let productsRequest = SKProductsRequest(productIdentifiers: ["hodr_yearly_membership"])
+        productsRequest.delegate = self
+        productsRequest.start()
+    }
+
+    func requestDidFinish(_ request: SKRequest) {
+        print("finished!")
+    }
+
+    func request(_ request: SKRequest, didFailWithError error: Error) {
+        print("errored!")
+        print(error)
+    }
+
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        print(response)
     }
 
 
